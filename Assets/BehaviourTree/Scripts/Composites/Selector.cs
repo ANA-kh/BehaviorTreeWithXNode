5a5a0e4c87c6eb1d1f22a8532e6752d91e6f6ac6
ANA_h <1154073432@qq.com>
2023-02-11 21:49:37 +0800

    add some Composites

---
+++ Assets/BehaviourTree/Scripts/Composites/Selector.cs
@@ -0,0 +1,38 @@
+﻿namespace BehaviourTree
+{
+    [System.Serializable]
+    public class Selector : CompositeNode
+    {
+        protected int currentChild = 0;
+        
+        protected override void OnStart()
+        {
+            currentChild = 0;
+        }
+        
+        protected override void OnStop()
+        {
+        }
+        
+        protected override State OnUpdate()
+        {
+            for (int i = currentChild; i < children.Count; ++i)
+            {
+                currentChild = i;
+                var child = children[currentChild];
+                
+                switch (child.Update())
+                {
+                    case State.Running:
+                        return State.Running;
+                    case State.Success:
+                        return State.Success;
+                    case State.Failure:
+                        continue;
+                }
+            }
+            
+            return State.Failure;
+        }
+    }
+}
---
+++ Assets/BehaviourTree/Scripts/Composites/Sequencer.cs
@@ -0,0 +1,38 @@
+namespace BehaviourTree
+{
+    [System.Serializable]
+    public class Sequencer : CompositeNode
+    {
+        protected int currentChild = 0;
+        
+        protected override void OnStart()
+        {
+            currentChild = 0;
+        }
+        
+        protected override void OnStop()
+        {
+        }
+        
+        protected override State OnUpdate()
+        {
+            for (int i = currentChild; i < children.Count; ++i)
+            {
+                currentChild = i;
+                var child = children[currentChild];
+                
+                switch (child.Update())
+                {
+                    case State.Running:
+                        return State.Running;
+                    case State.Failure:
+                        return State.Failure;
+                    case State.Success:
+                        continue;
+                }
+            }
+            
+            return State.Success;
+        }
+    }
+}
