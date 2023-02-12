9bd58eab8c43d88f23cc6044835aa85111606aa5
ANA_h <1154073432@qq.com>
2023-02-12 14:12:59 +0800

    Decorator

---
+++ Assets/BehaviourTree/Scripts/Decorators/Repeat.cs
@@ -0,0 +1,27 @@
+﻿namespace BT.Decorators
+{
+    public class Repeat : DecoratorNode
+    {
+        public int repeatCount = 1;
+        private int currentCount = 0;
+
+        protected override void OnStart()
+        {
+            currentCount = 0;
+        }
+
+        protected override State OnUpdate()
+        {
+            if (currentCount < repeatCount)
+            {
+                currentCount++;
+                child.Update();
+                return State.Running;
+            }
+            else
+            {
+                return State.Success;
+            }
+        }
+    }
+}
