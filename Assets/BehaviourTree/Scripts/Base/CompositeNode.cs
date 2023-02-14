787bc8074899ef478a5a41916b68411d34df6327
ANA_h <1154073432@qq.com>
2023-02-14 11:35:26 +0800

    fix abort right

--- Assets/BehaviourTree/Scripts/Base/CompositeNode.cs
+++ Assets/BehaviourTree/Scripts/Base/CompositeNode.cs
@@ -51,6 +51,7 @@ namespace BT
             {
                 children[i].Abort();
             }
+            parent.AbortRight(indexInParent);
         }
 
         internal override void OnObserverBegin()
