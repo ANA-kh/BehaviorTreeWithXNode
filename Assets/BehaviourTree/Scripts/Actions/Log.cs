2acaa0d2e6834c3e9018cc188a1a4b0fc507298d
ANA_h <1154073432@qq.com>
2023-02-14 10:38:52 +0800

    fix log

--- Assets/BehaviourTree/Scripts/Actions/Log.cs
+++ Assets/BehaviourTree/Scripts/Actions/Log.cs
@@ -2,14 +2,27 @@ using UnityEngine;
 
 namespace BT.Actions
 {
+    [System.Serializable]
     public class Log : ActionNode
     {
+        public int Count;
         public string message;
+        private int _curCount;
+
+        protected override void OnStart()
+        {
+            _curCount = 0;
+        }
 
         protected override State OnUpdate()
         {
+            if (_curCount >= Count)
+            {
+                return State.Success;
+            }
             Debug.Log(message);
-            return State.Success;
+            _curCount++;
+            return State.Running;
         }
     }
 }
